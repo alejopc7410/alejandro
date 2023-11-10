08eb083919efaf62966ba3e90e0b833d3e8e45b1
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2023-11-10 15:52:50 -0600

    Finishing the web design and starting the responsive design

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,2 +1,41 @@
 'use strict';
 
+/*------------ Utility Functions ----------------*/
+
+function select (selector) {
+    return document.querySelector(selector);
+}
+
+function selectAll (selector) {
+    return document.querySelectorAll(selector);
+}
+
+function selectById (selector) {
+    return document.getElementById(selector);
+}
+
+function onEvent (selector, event, callback) {
+    return selector.addEventListener(event, callback);
+}
+/*-----------------------------------------------*/
+
+/*---------------- HTML Elements ----------------*/
+const li1 = select('li:nth-child(1)')
+const li2 = select('li:nth-child(2)')
+const li3 = select('li:nth-child(3)')
+const home = select('.banner')
+const aboutMe = select('.about-me')
+const getInTouch = select('.get-in-touch')
+/*-----------------------------------------------*/
+
+onEvent(li1, 'click', () => {
+    home.scrollIntoView({ behavior: 'smooth' });
+})
+
+onEvent(li2, 'click', () => {
+    aboutMe.scrollIntoView({ behavior: 'smooth' });
+})
+
+onEvent(li3, 'click', () => {
+    getInTouch.scrollIntoView({ behavior: 'smooth' });
+})
